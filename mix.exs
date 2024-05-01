8ef7947c34d3c856bbc63c4da7ca9ed4e7791174
Travis Hinkelman <thinkelman@pm.me>
2024-04-30 22:52:31 -0700

    Add dialyxir

--- mix.exs
+++ mix.exs
@@ -32,6 +32,7 @@ defmodule Jokers.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:dialyxir, "~> 1.4.3", only: [:dev], runtime: false},
       {:phoenix, "~> 1.7.12"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.10"},
